cf04554f66399b6efbc775f4c9cd6bc655964959
Johnny Domino <johnny@yayuhh.com>
2015-05-27 15:47:19 -0400

    replace file serving with proxying

---
+++ plugins/api-proxy.js
@@ -0,0 +1,37 @@
+const info = require('../package.json');
+const conf = require('../config');
+const _ = require('lodash');
+
+exports.register = function (server, options, next) {
+
+  var mapper = function (request, callback) {
+
+    var username = 'herokai';
+    var password = 'ohherrotherenicetoseeyou';
+    var basic = 'Basic ' + (new Buffer(username + ':' + password, 'utf8')).toString('base64');
+    callback(null, `http://purple.herokuapp.com${request.url.path}`, { authorization: basic });
+  };
+
+
+  server.route({
+    method: '*',                            // all methods
+    path: '/{path*}',                       // all routes prefixed with `/api/`
+    config: { auth: 'session' },
+    handler: {
+      proxy: {
+        passThrough: true,                  // forwards the headers sent from the client to the upstream service
+        xforward: true,                     // sets the 'X-Forwarded-For', 'X-Forwarded-Port', 'X-Forwarded-Proto' headers
+        timeout: conf.get('/http/timeout'), // number of milliseconds before aborting the upstream request
+        mapUri: mapper
+      }
+    }
+  });
+
+  next();
+};
+
+exports.register.attributes = {
+  name: 'proxy',
+  version: info.version,
+  multiple: true
+};
